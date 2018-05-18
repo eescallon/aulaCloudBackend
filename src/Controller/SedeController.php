4a737c9a6f1ddd03dcfb0bfa869cdd96f7e14fe1
CAHOYOS958 <38771331+CAHOYOS958@users.noreply.github.com>
2018-05-17 21:28:29 -0500

    versedes

--- src/Controller/SedeController.php
+++ src/Controller/SedeController.php
@@ -1,8 +1,37 @@
 <?php
 namespace App\Controller;
+use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Symfony\Component\HttpFoundation\Request;
+use App\Entity\User;
+use App\Entity\Sede;
 
-class SedeController
+class SedeController extends Controller
 {
+	/**
+     * @Route("/insertarsede", name="insertarsede")
+     */
+    public function index()
+    {   
+        $em = $this->getDoctrine()->getManager();
+        $sede = new Sede();
+        $sede->setAddress("Direccion");
+        $sede->setCity("Ciutdad");
+        $em->persist($sede);
+        $em->flush();
+       	return $this->json(array("success" => true, "data" => array()));
+    }
+
+ 	/**
+     * @Route("/sede", name="consultasede")
+     */
+    public function getAllSede()
+    {   
+        $em = $this->getDoctrine()->getManager();
+        $repoSede = $this->getDoctrine()->getRepository(Sede::class);
+        $sedes = $repoSede->findAll();
+       	return $this->json(array("success" => true, "data" => $sedes));
+    }
 }
 
 ?>
