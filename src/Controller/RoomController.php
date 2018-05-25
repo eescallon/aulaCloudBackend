790f639ecd9556443c81c47fc69d18ed22c440eb
Hectorfa <kilian881312@gmail.com>
2018-05-24 19:20:32 -0500

    traer areas

--- src/Controller/RoomController.php
+++ src/Controller/RoomController.php
@@ -1,12 +1,37 @@
 <?php
 namespace App\Controller;
+use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Component\Routing\Annotation\Method;
+use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Symfony\Component\Serializer\Encoder\JsonEncoder;
+use App\Entity\Room;
+use App\Entity\Sede;
+use App\Entity\User;
 
-class RoomController
+class RoomController extends Controller
 {
-	public function login()
-	{
-		
-	}
+       /**
+     * @Route("/consult/room/{idSede}", name="Consulter_room")
+     */
+    public function consultRoom($idSede)
+    {   
+        $em = $this->getDoctrine()->getManager();
+        $repoRoom = $this->getDoctrine()->getRepository(Room::class);
+        $repoSede = $this->getDoctrine()->getRepository(Sede::class);
+        $sede = $repoSede->find($idSede);
+        $rooms = $repoRoom->findBy(["idSede" => $sede]);
+        $array = array();
+        foreach($rooms as $room)
+        {
+            $temp = array();
+            $temp["id"] = $room->getId();
+            $temp["name"] = $room->getName();
+            $temp["code"] = $room->getCode();
+            $temp["type"] = $room->getType();
+            $temp["capacity"] = $room->getCapacity();
+            $array[] = $temp;
+        }
+        return.$this->json(array("success" => true, "data" => $array));
+    }
 }
-
 ?>
